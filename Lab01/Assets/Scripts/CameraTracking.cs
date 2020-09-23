98b38f59b9e67fcf5a7575407c3d3bc6f96c3aed
aaronyuen2 <35553648+aaronyuen2@users.noreply.github.com>
2020-09-23 21:57:46 +0800

    unity project added
    Unity Source Project Added (Day 1)

---
+++ Lab01/Assets/Scripts/CameraTracking.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTracking : MonoBehaviour
+{
+    public Transform target;
+    bool follow = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (follow)
+        {
+            //transform.LookAt(target);
+            transform.position = new Vector3(
+                target.position.x,
+                target.position.y + 2,
+                target.position.z - 10);
+
+            Camera.main.transform.rotation = target.transform.rotation;
+        }     
+        // lose camera
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position = transform.position;
+            follow = false;
+        }
+    }
+}
---
+++ Lab01/Assets/Scripts/Controls.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    Vector3 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // detect if keyboard has pressed any key?
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            pos.x -= 0.4f;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            pos.x += 0.4f;
+        }
+
+        //up
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            pos.z += 0.4f;
+        }
+
+        // down
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            pos.z -= 0.4f;
+        }
+
+        // rotate left
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0, 0, 1);
+        }
+
+        // rotate right
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0, 0, -1);
+        }
+
+        
+
+
+        this.transform.position = pos;
+    }
+}
